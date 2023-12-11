0db7f4a23730fbc2d5df890aa3b5365fa4b2e60b
Izuchii1311 <luthfiramadhan.lr55@gmail.com>
2023-12-11 22:06:48 +0700

    Landing Page

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,28 @@
+{{-- Main Layouts --}}
+@extends('main.layouts.main')
+
+@section('container')
+    {{-- Hero Section --}}
+    @include('main.layouts.partials.hero')
+    {{-- End Hero Section --}}
+
+    {{-- Main Section --}}
+    <main id="main">
+        @include('main.layouts.partials.about')
+
+        @include('main.layouts.partials.services')
+
+        @include('main.layouts.partials.counts')
+
+        @include('main.layouts.partials.cta')
+
+        @include('main.layouts.partials.portofolio')
+
+        @include('main.layouts.partials.testimonials')
+
+        @include('main.layouts.partials.team')
+
+        @include('main.layouts.partials.contact')
+    </main>
+    {{-- End Main Section --}}
+@endsection
