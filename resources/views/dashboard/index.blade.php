7c01b59eeb2341b31801bba1b9a58b96d1e72174
Izuchii1311 <luthfiramadhan.lr55@gmail.com>
2023-12-12 10:03:54 +0700

    View Dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,64 @@
+@extends('dashboard.layouts.main')
+
+@section('content')
+<!-- Content -->
+<div class="container-xxl flex-grow-1 container-p-y">
+    {{-- Cards --}}
+    <div class="row">
+        {{-- Congratulation Card --}}
+        @include('dashboard.layouts.partials.congratulation-card')
+        {{-- End Congratualation Card --}}
+
+        <div class="col-lg-4 col-md-4 order-1">
+            <div class="row">
+                {{-- Card Profit --}}
+                @include('dashboard.layouts.partials.card-profit')
+                {{-- End Card Profit --}}
+                {{-- Card Sales --}}
+                @include('dashboard.layouts.partials.card-sales')
+                {{-- End Card Sales --}}
+            </div>
+        </div>
+
+        <!-- Total Revenue -->
+        @include('dashboard.layouts.partials.card-total-revenue')
+        <!--/ Total Revenue -->
+
+        <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+            <div class="row">
+                {{-- Card Payment --}}
+                @include('dashboard.layouts.partials.card-payment')
+                {{-- End Card Payment --}}
+                {{-- Card Transaction --}}
+                @include('dashboard.layouts.partials.card-transaction')
+                {{-- End Card Transaction --}}
+            </div>
+
+            {{-- Card Profile Report --}}
+            @include('dashboard.layouts.partials.card-profile-report')
+            {{-- End Card Profile Report --}}
+
+        </div>
+        {{-- End Col --}}
+
+    </div>
+    {{-- End Cards --}}
+
+    {{-- Cards --}}
+    <div class="row">
+        <!-- Order Statistics -->
+        @include('dashboard.layouts.partials.card-order-statistic')
+        <!--/ Order Statistics -->
+
+        <!-- Expense Overview -->
+        @include('dashboard.layouts.partials.card-expense-overview')
+        <!--/ Expense Overview -->
+
+        <!-- Transactions -->
+        @include('dashboard.layouts.partials.card-transactions')
+        <!--/ Transactions -->
+    </div>
+    {{-- End Cards --}}
+</div>
+<!-- / Content -->
+@endsection
---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-menu-fixed layout-compact" dir="ltr" data-theme="theme-default" data-template="vertical-menu-template-free">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('/img/dashboard/favicon/favicon.ico') }}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('/vendor/dashboard/fonts/boxicons.css') }}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('/vendor/dashboard/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('/vendor/dashboard/css/theme-default.css') }}" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('/css/dashboard/demo.css') }}" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('/vendor/dashboard/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/vendor/dashboard/libs/apex-charts/apex-charts.css') }}" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="{{ asset('/vendor/dashboard/js/helpers.js') }}"></script>
+    <script src="{{ asset('/js/dashboard/config.js') }}"></script>
+</head>
+
+<body>
+<!-- Layout wrapper -->
+<div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+        {{-- Sidebar Section --}}
+        @include('dashboard.layouts.sidebar')
+        {{-- End Sidebar Section --}}
+
+        <!-- Layout container -->
+        <div class="layout-page">
+
+            {{-- Navbar Section --}}
+            @include('dashboard.layouts.navbar')
+            {{-- End Navbar Section --}}
+
+            <!-- Content wrapper -->
+            <div class="content-wrapper">
+                <!-- Content -->
+                @yield('content')
+                <!-- / Content -->
+
+                {{-- Footer Section --}}
+                @include('dashboard.layouts.footer')
+                {{-- End Footer Section --}}
+
+                <div class="content-backdrop fade"></div>
+            </div>
+        <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+</div>
+<!-- / Layout wrapper -->
+
+    <div class="buy-now">
+        <a href="https://themeselection.com/item/sneat-bootstrap-html-admin-template/" target="_blank" class="btn btn-danger btn-buy-now" >Upgrade to Pro</a >
+    </div>
+
+    <!-- Core JS -->
+    <!-- build:{{ asset('') }}/vendor/js/core.js -->
+
+    <script src="{{ asset('/vendor/dashboard/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('/vendor/dashboard/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('/vendor/dashboard/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('/vendor/dashboard/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('/vendor/dashboard/js/menu.js') }}"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ asset('/vendor/dashboard/libs/apex-charts/apexcharts.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ asset('/js/dashboard/main.js') }}"></script>
+
+    <!-- Page JS -->
+    <script src="{{ asset('/js/dashboard/dashboards-analytics.js') }}"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+</html>
